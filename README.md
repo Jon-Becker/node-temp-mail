86606a8355186902120fc5234638319d4271aeda
Jonathan Becker <64037729+Jon-Becker@users.noreply.github.com>
2020-07-12 21:07:11 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# node-temp-mail
- Temporary node wrapper for temporary emails.
+node-temp-mail
+===================
+
+Node wrapper for creating and fetching temporary, disposable emails, as well as their new messages.
+
+#### Installation
+```
+npm install node-temp-mail
+```
+
+#### Usage
+```
+var TempMail = require('node-temp-mail');
+
+// Let's create an address object so it can be accessed by the module.
+var address = new TempMail("testAddress");
+
+// We already have the address object, so now let's access it and get a list of the emails in a nice & neat json object.
+address.fetchEmails(function(err,body){
+  console.log(body);
+});
+
+// If for any reason you need to see the full temporary email address, you can use the following function.
+address.getAddress()
+```
---
+++ index.js
@@ -0,0 +1,66 @@
+var request = require('request');
+
+function buildJson(callback, label, currentJson, addressJson, currentIndex, finalIndex) {
+
+  request({
+    url: "https://www.1secmail.com/api/v1/?action=readMessage&login="+label+"&domain=1secmail.com&id="+addressJson[currentIndex].id,
+    json: true
+  }, function (error, response, body) {
+    if (error || response.statusCode !== 200) {
+      return callback(error || {statusCode: response.statusCode});
+    }
+    else {
+      var messageBody = {}
+      messageBody.from = body.from;
+      messageBody.timestamp = body.date;
+      messageBody.subject = body.subject;
+      messageBody.message = body.textBody;
+      currentJson.messages.push(messageBody);
+      if((currentIndex+1) < finalIndex){
+        buildJson(label, currentJson, addressJson, (currentIndex+1), finalIndex);
+      }
+      else {
+        callback(null, currentJson);
+      }
+    }
+  });
+}
+
+class TempMail {
+  constructor(mailingAddressLabel) {
+    this.mailingAddressLabel = mailingAddressLabel;
+  }
+
+  fetchEmails(callback){
+    var isComplete = false;
+    var mailingAddressLabel = this.mailingAddressLabel;
+
+
+    request({
+      url: "https://www.1secmail.com/api/v1/?action=getMessages&login="+mailingAddressLabel+"&domain=1secmail.com",
+      json: true
+    }, function (error, response, body) {
+      if (error || response.statusCode !== 200) {
+        return callback(error || {statusCode: response.statusCode});
+      }
+      else {
+        response = {};
+        response.address = mailingAddressLabel + "@1secmail.com";
+        response.messageCount = body.length;
+        response.messages = [];
+
+        buildJson(callback, mailingAddressLabel, response, body, 0, body.length);
+      }
+    });
+
+
+  }
+
+  getAddress(){
+    var response = {};
+    response.address = this.mailingAddressLabel + "@1secmail.com";
+    return response;
+  }
+}
+
+module.exports = TempMail;
