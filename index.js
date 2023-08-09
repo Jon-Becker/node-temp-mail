d807eaaf8e4acc0bac7ee33ada3e24ba9592ca68
a-rustacean <dilshadplayingminecraft@outlook.com>
2023-08-09 11:23:42 +0530

    Add axios, Use async/await Add JSDoc comments and Remove request

--- index.js
+++ index.js
@@ -1,70 +1,65 @@
-var request = require('request');
+const { default: axios } = require("axios");
+const nodify = require("./nodify");
 
-function buildJson(callback, label, currentJson, addressJson, currentIndex, finalIndex) {
-
-  request({
-    url: "https://www.1secmail.com/api/v1/?action=readMessage&login="+label+"&domain=1secmail.com&id="+addressJson[currentIndex].id,
-    json: true
-  }, function (error, response, body) {
-    if (error || response.statusCode !== 200) {
-      return callback(error || {statusCode: response.statusCode});
-    }
-    else {
-      var messageBody = {}
-      messageBody.from = body.from;
-      messageBody.timestamp = body.date;
-      messageBody.subject = body.subject;
-      messageBody.message = body.textBody;
-      currentJson.messages.push(messageBody);
-      if((currentIndex+1) < finalIndex){
-        buildJson(callback, label, currentJson, addressJson, (currentIndex+1), finalIndex);
-      }
-      else {
-        callback(null, currentJson);
-      }
-    }
-  });
-}
+/** @typedef {{ from: string, timestaml: string, subject: string, message: string }} */
+/** @typedef {{ address: string, messageCount: number, messages: Message[] }} EmailFetchResult */
 
 class TempMail {
+  /** @type {string} */
+  mailingAddressLabel;
+
+  /**
+   * create a new instance of TempMail
+   * @param {string} mailingAddressLabel mailing address label
+   */
   constructor(mailingAddressLabel) {
     this.mailingAddressLabel = mailingAddressLabel;
   }
 
-  fetchEmails(callback){
-    var isComplete = false;
-    var mailingAddressLabel = this.mailingAddressLabel;
-
-
-    request({
-      url: "https://www.1secmail.com/api/v1/?action=getMessages&login="+mailingAddressLabel+"&domain=1secmail.com",
-      json: true
-    }, function (error, response, body) {
-      if (error || response.statusCode !== 200) {
-        return callback(error || {statusCode: response.statusCode});
+  /**
+   * fetchs emails and returns it
+   * @returns {Promise<EmailFetchResult>}
+   */
+  async #fetchEmails() {
+    const httpResponse = await axios.get("https://www.1secmail.com/api/v1/?action=getMessages&login=" + this.mailingAddressLabel + "&domain=1secmail.com");
+    const data = httpResponse.data;
+    const messages = await Promise.all(data.map(async (email) => {
+      const mailHttpResponse = await axios.get("https://www.1secmail.com/api/v1/?action=readMessage&login=" + label + "&domain=1secmail.com&id=" + email.id);
+      const mailData = mailHttpResponse.data;
+      return {
+        from: mailData.from,
+        timestamp: mailData.date,
+        subject: mailData.subject,
+        message: mailData.textBody
       }
-      else {
-        response = {};
-        response.address = mailingAddressLabel + "@1secmail.com";
-        response.messageCount = body.length;
-        response.messages = [];
-        if(body.length > 0){
-          buildJson(callback, mailingAddressLabel, response, body, 0, body.length);
-        }
-        else {
-          callback(null, response);
-        }
-      }
-    });
-
-
+    }));
+    const res = {
+      address: this.mailingAddressLabel + "@1secmail.com",
+      messageCount: data.length,
+      messages
+    };
+    return res;
   }
 
-  getAddress(){
-    var response = {};
-    response.address = this.mailingAddressLabel + "@1secmail.com";
-    return response;
+  /**
+   * fetchs emails and calls the callback if not provided just returns a promise
+   * @param {import("./nodify").callback<EmailFetchResult, Error>} callback callback
+   * @returns {Promise<EmailFetchResult>}
+   */
+  fetchEmails(callback) {
+    return nodify(this.#fetchEmails(), callback);
+  }
+
+  /**
+   * returns the mailing address label
+   * @returns {{ address: string }}
+   */
+  getAddress() {
+    return {
+      address: this.mailingAddressLabel + "@1secmail.com"
+    }
   }
 }
 
 module.exports = TempMail;
+
