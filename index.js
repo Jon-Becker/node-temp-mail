4f6ba27e963d104f9ddd207e31fa3b81990abbf4
a-rustacean <dilshadplayingminecraft@outlook.com>
2023-08-13 19:08:47 +0530

    Format `index.js`

--- index.js
+++ index.js
@@ -21,22 +21,33 @@ class TempMail {
    * @returns {Promise<EmailFetchResult>}
    */
   async #fetchEmails() {
-    const httpResponse = await axios.get("https://www.1secmail.com/api/v1/?action=getMessages&login=" + this.mailingAddressLabel + "&domain=1secmail.com");
+    const httpResponse = await axios.get(
+      "https://www.1secmail.com/api/v1/?action=getMessages&login=" +
+        this.mailingAddressLabel +
+        "&domain=1secmail.com",
+    );
     const data = httpResponse.data;
-    const messages = await Promise.all(data.map(async (email) => {
-      const mailHttpResponse = await axios.get("https://www.1secmail.com/api/v1/?action=readMessage&login=" + label + "&domain=1secmail.com&id=" + email.id);
-      const mailData = mailHttpResponse.data;
-      return {
-        from: mailData.from,
-        timestamp: mailData.date,
-        subject: mailData.subject,
-        message: mailData.textBody
-      }
-    }));
+    const messages = await Promise.all(
+      data.map(async (email) => {
+        const mailHttpResponse = await axios.get(
+          "https://www.1secmail.com/api/v1/?action=readMessage&login=" +
+            label +
+            "&domain=1secmail.com&id=" +
+            email.id,
+        );
+        const mailData = mailHttpResponse.data;
+        return {
+          from: mailData.from,
+          timestamp: mailData.date,
+          subject: mailData.subject,
+          message: mailData.textBody,
+        };
+      }),
+    );
     const res = {
       address: this.mailingAddressLabel + "@1secmail.com",
       messageCount: data.length,
-      messages
+      messages,
     };
     return res;
   }
@@ -56,10 +67,9 @@ class TempMail {
    */
   getAddress() {
     return {
-      address: this.mailingAddressLabel + "@1secmail.com"
-    }
+      address: this.mailingAddressLabel + "@1secmail.com",
+    };
   }
 }
 
 module.exports = TempMail;
-
